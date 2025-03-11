3556a6c8129e090467836a1195a25387051ab3ab
@estebanroseroo <estebanrosero_95@hotmail.com>
2025-03-10 20:46:27 -0400

    Modify server to handling better the routes for all features

--- myAppBackend/server.js
+++ myAppBackend/server.js
@@ -16,7 +16,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
 const routes = require('./routes/routes');
-app.use('/api/symptoms', routes);
+app.use('/api/myApp', routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
