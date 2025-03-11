759217dba7e90e08f952686db7ae317fd9d1b63b
@estebanroseroo <estebanrosero_95@hotmail.com>
2025-03-10 20:43:35 -0400

    Add routes in the server to work with all features

--- myAppBackend/server.js
+++ myAppBackend/server.js
@@ -15,5 +15,8 @@ app.get('/', (req, res) => res.send('API is running...'));
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
+const routes = require('./routes/routes');
+app.use('/api/symptoms', routes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
