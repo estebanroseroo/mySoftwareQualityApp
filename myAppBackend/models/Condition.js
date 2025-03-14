514add939fcbfeb95384775b5e45aebdbb42902b
@estebanroseroo <estebanrosero_95@hotmail.com>
2025-03-13 21:00:57 -0400

    Update condition model for adding feedback as the continuous learning feature

--- myAppBackend/models/Condition.js
+++ myAppBackend/models/Condition.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 const conditionSchema = new mongoose.Schema({
     name: { type: String, required: true },
     symptoms: { type: [String], required: true },
-});
+    feedback: [
+        {
+            userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+            helpful: { type: Boolean },
+            comment: { type: String }
+        }
+    ],
+}, { timestamps: true });
 
 module.exports = mongoose.model('Condition', conditionSchema);
